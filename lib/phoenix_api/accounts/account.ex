54b82fce1a5476294bcb36a7a3819b5c8ae1596f
Riszkhy <riszkhy@gmail.com>
2023-06-01 19:41:26 +0700

    api update user
    - update user

--- lib/phoenix_api/accounts/account.ex
+++ lib/phoenix_api/accounts/account.ex
@@ -23,7 +23,19 @@ defmodule PhoenixApi.Accounts.Account do
     |> put_password_hash()
   end
 
+  def changeset_update(account, attrs) do
+    account
+    |> cast(attrs, [:email, :hash_password])
+    |> validate_required([:email, :hash_password])
+    |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "must have the @ sign")
+    |> unique_constraint(:email)
+    |> put_password_hash()
+  end
+
+
   defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{hash_password: hash_password}} = changeset ) do
     change(changeset, hash_password: Pbkdf2.hash_pwd_salt(hash_password))
   end
+
+  defp put_password_hash(changeset), do: changeset
 end
--- lib/phoenix_api_web/auth/error_response.ex
+++ lib/phoenix_api_web/auth/error_response.ex
@@ -1,3 +1,7 @@
 defmodule PhoenixApiWeb.Auth.ErrorResponse.Unauthorized do
   defexception [message: "Unauthorized", plug_status: 401]
 end
+
+defmodule PhoenixApiWeb.Auth.ErrorResponse.Forbidden do
+  defexception [message: "Forbidden Access", plug_status: 403]
+end
